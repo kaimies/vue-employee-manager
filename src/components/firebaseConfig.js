aea5bc823b6ecca41e9f18cc77bd9b498b977600
Kai Mies <develop@regnavi.de>
2017-12-24 13:20:29 +0100

    Initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyAqyBifSFb9vb8NV_i22KFIPm2T4TOE_pc',
+  authDomain: 'vuefs-prod-387a4.firebaseapp.com',
+  databaseURL: 'https://vuefs-prod-387a4.firebaseio.com',
+  projectId: 'vuefs-prod-387a4',
+  storageBucket: 'vuefs-prod-387a4.appspot.com',
+  messagingSenderId: '506707012134',
+};
